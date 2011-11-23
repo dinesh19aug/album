78040bdba6a2999442620dcd1085d396039efdf0
Dinesh Arora <dinesh19aug@gmail.com>
2011-11-23 00:10:31 -0500

    Added authentication using sorcery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  helper_method :current_users_list
+    protected
+    def current_users_list
+      current_users.map {|u| u.email}.join(", ")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Album::Application.routes.draw do
+  get "admin/index"
+  
+  get "logout" => "sessions#destroy", :as => "logout"
+    get "login" => "sessions#new", :as => "login"
+    get "signup" => "users#new", :as => "signup"
+    resources :users
+    resources :sessions
+    get "secret" => "home#secret", :as => "secret"
+    root :to => 'admin#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
