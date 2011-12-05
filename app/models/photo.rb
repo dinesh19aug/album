2e43b2afad9864a2a6babcecc80d0480d614bc3d
Dinesh Arora <dinesh19aug@gmail.com>
2011-12-04 21:55:06 -0500

    Added validation on photo model to ensure presence of all the fileds

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,15 @@
 class Photo < ActiveRecord::Base
+
+#Attributes or fileds
   attr_accessible :image,:pic_name,:description,:albums_id
 
+#associations
   belongs_to :albums
   
+#carrier wave  
   mount_uploader :image,ImageUploader
+
+#Validations
+validates :description,:pic_name,:albums_id, :presence=>true
+
 end
