001df073bd045603e898e231ad6fd6c6bfefa690
Dinesh Arora <dinesh19aug@gmail.com>
2012-04-08 16:42:10 -0400

    Some more chnages

--- app/assets/javascripts/test.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".myform").bind("ajax:loading",function(evt,xhr,settings){
-       var $ent=$(this);
-       $ent.find(("div.yes")).hide(100);
-
-    }
-    }
-);
