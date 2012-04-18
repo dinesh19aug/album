4551e9ead88fac3a2e1611b62336c3c79de1cd95
Dinesh Arora <dinesh19aug@gmail.com>
2012-04-18 19:47:47 -0400

    removed galleria plugin

--- public/assets/galleria/plugins/history/galleria.history.min-f52b1fd258d7e7bf344201a1f1e08c90.js
+++
@@ -1,9 +0,0 @@
-/*
- Galleria History Plugin 2011-08-01
- http://galleria.aino.se
-
- Copyright 2011, Aino
- Licensed under the MIT license.
-
-*/
-Galleria.requires(1.25,"The History Plugin requires Galleria version 1.2.5 or later."),function(a,b){Galleria.History=function(){var d=[],f=!1,g=b.location,h=b.document,i=Galleria.IE,j="onhashchange"in b&&(h.mode===void 0||h.mode>7),k,l=function(a){return a=k&&!j&&Galleria.IE?a||k.location:g,parseInt(a.hash.substr(2),10)},m=l(g),n=[],o=function(){a.each(n,function(a,d){d.call(b,l())})},p=function(){a.each(d,function(a,b){b()}),f=!0};return j&&i<8&&(j=!1),j?p():a(function(){b.setInterval(function(){var a=l();!isNaN(a)&&a!=m&&(m=a,g.hash="/"+a,o())},50),i?a('<iframe tabindex="-1" title="empty">').hide().attr("src","about:blank").one("load",function(){k=this.contentWindow,p()}).insertAfter(h.body):p()}),{change:function(a){n.push(a),j&&(b.onhashchange=o)},set:function(a){isNaN(a)||(!j&&i&&this.ready(function(){var b=k.document;b.open(),b.close(),k.location.hash="/"+a}),g.hash="/"+a)},ready:function(a){f?a():d.push(a)}}}()}(jQuery,this)
--- public/assets/galleria/themes/classic/galleria.classic.min-39b8a4adf6f4aa3b0283d598f6604d0f.js
+++
@@ -1,8 +0,0 @@
-/*
- Galleria Classic Theme 2011-08-01
- http://galleria.aino.se
-
- Copyright (c) 2011, Aino
- Licensed under the MIT license.
-*/
-Galleria.requires(1.25,"This version of Classic theme requires Galleria 1.2.5 or later"),function(a){Galleria.addTheme({name:"classic",author:"Galleria",css:"galleria.classic.css",defaults:{transition:"slide",thumbCrop:"height",_toggleInfo:!0},init:function(c){this.addElement("info-link","info-close"),this.append({info:["info-link","info-close"]});var d=this.$("info-link,info-close,info-text"),e=Galleria.TOUCH,f=e?"touchstart":"click";this.$("loader,counter").show().css("opacity",.4),e||(this.addIdleState(this.get("image-nav-left"),{left:-50}),this.addIdleState(this.get("image-nav-right"),{right:-50}),this.addIdleState(this.get("counter"),{opacity:0})),c._toggleInfo===!0?d.bind(f,function(){d.toggle()}):(d.show(),this.$("info-link, info-close").hide()),this.bind("thumbnail",function(c){e?a(c.thumbTarget).css("opacity",this.getIndex()?1:.6):(a(c.thumbTarget).css("opacity",.6).parent().hover(function(){a(this).not(".active").children().stop().fadeTo(100,1)},function(){a(this).not(".active").children().stop().fadeTo(400,.6)}),c.index===this.getIndex()&&a(c.thumbTarget).css("opacity",1))}),this.bind("loadstart",function(c){c.cached||this.$("loader").show().fadeTo(200,.4),this.$("info").toggle(this.hasInfo()),a(c.thumbTarget).css("opacity",1).parent().siblings().children().css("opacity",.6)}),this.bind("loadfinish",function(){this.$("loader").fadeOut(200)})}})}(jQuery)
